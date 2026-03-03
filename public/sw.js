b5f43c5520b23d79a71427df55b71bac5b918b99
ANIRUDDH-001 <aniruddhvijay2k7@gmail.com>
2026-03-03 09:41:38 +0530

    Schema bug fix

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // AlgoMind Service Worker
 // Provides offline caching for PWA functionality
 
-const CACHE_NAME = 'algomind-1772453418352';
+const CACHE_NAME = 'algomind-1772510936320';
 const STATIC_ASSETS = [
     '/',
     '/manifest.webmanifest',
