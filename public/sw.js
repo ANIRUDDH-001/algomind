c25625a58bee23f338ba4da2ca9e853982dcb9b2
ANIRUDDH-001 <aniruddhvijay2k7@gmail.com>
2026-03-08 19:26:08 +0530

    Update docs with AWS Bedrock and benchmarking details

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // AlgoMind Service Worker
 // Provides offline caching for PWA functionality
 
-const CACHE_NAME = 'algomind-1772871653116';
+const CACHE_NAME = 'algomind-1772969946513';
 const STATIC_ASSETS = [
     '/',
     '/manifest.webmanifest',
