f2004eec0c80776e78ea5dcf6a626a61a328a8fb
ANIRUDDH-001 <aniruddhvijay2k7@gmail.com>
2026-03-09 22:18:44 +0530

    mic and analysis

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // AlgoMind Service Worker
 // Provides offline caching for PWA functionality
 
-const CACHE_NAME = 'algomind-1773047712235';
+const CACHE_NAME = 'algomind-1773074824318';
 const STATIC_ASSETS = [
     '/',
     '/manifest.webmanifest',
